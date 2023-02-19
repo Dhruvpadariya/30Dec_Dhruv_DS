528a04ba2d7a60b6ba433fe798c90f86c24f367e
Dhruvpadariya <121600901+Dhruvpadariya@users.noreply.github.com>
2023-02-19 14:37:16 +0530

    Delete 2.py

--- 2.py
+++
@@ -1,8 +0,0 @@
-# Python Program to Count Occurrences of a Substring in a String
-
-my_str=input('Enter a String : ')
-sub_str=input('Enter a Substring : ')
-
-my_count=my_str.count(sub_str)
-
-print('Substring occurs %d times' % my_count)
