71a67bf1455ed66f641a079e33a78d047544dceb
Dhruvpadariya <121600901+Dhruvpadariya@users.noreply.github.com>
2023-02-19 14:37:32 +0530

    Delete 4.py

--- 4.py
+++
@@ -1,8 +0,0 @@
-#Python program to get a single string from two given strings, separated by a space and swap the first two characters of each string
-
-str_1='abcd'
-str_2='wxyz'
-temp=str_2
-str_2=str_1[0:2] + str_2[2:]
-str_1= temp[0:2] + str_1[2:]
-print(str_2 +' ' + str_1)
